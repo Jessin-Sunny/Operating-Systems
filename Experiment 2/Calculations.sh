4fb0f7c7e69c90785a6d22172c82cdc1f7378a2e
Jessin Sunny <119120870+Jessin-Sunny@users.noreply.github.com>
2024-03-03 13:26:04 +0530

    Add files via upload

---
+++ Experiment 2/Calculations.sh
@@ -0,0 +1,43 @@
+
+#Program for Addition, Substraction, Multiplication, Division and Remainder
+
+ch="y"
+echo "1 : Addition"
+echo "2 : Substraction"
+echo "3 : Multiplication"
+echo "4 : Division"
+echo "5 : Remainder"
+echo "6 : Exit"
+while [ $ch == "y" ]
+do
+    echo "Enter your choice "
+    read value
+    echo "Enter the first number"
+    read a
+    echo "Enter the second number"
+    read b
+    case $value in
+        1)
+            r=$(( $a + $b ))
+            echo "Result : $r";;
+        2)
+            r=$(( $a - $b ))
+            echo "Result : $r";;
+        3)
+            r=$(( $a * $b ))
+            echo "Result : $r";;
+        4)
+            r=$(( $a / $b ))
+            echo "Result : $r";;
+        5)
+            r=$(( $a % $b ))
+            echo "Result : $r";;
+
+        *)
+            echo "Invalid Choice"
+            ;;
+        esac
+        echo "Do you want to continue ? "
+        read ch
+
+done
