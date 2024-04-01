6bf18fe506bb1fd979ffe1319b4579ed0bd3216d
Jessin Sunny <119120870+Jessin-Sunny@users.noreply.github.com>
2024-04-01 17:01:43 +0530

    Update User1.c

--- Experiment 6/User1.c
+++ Experiment 6/User1.c
@@ -1,56 +1,78 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/types.h>
-#include <unistd.h>
 
-struct shared_area
+#define SIZE 1000
+
+struct message
 {
+    int flag;
+    int turn;
     int user1_up;
     int user2_up;
-    int rw;
-    char msg[100];
+    char msg[SIZE];
 };
-
-struct shared_area* shmptr;
+struct message* mptr;
 
 int main()
 {
-    int shmid;
-    int key=520;
-    shmid=shmget(key,sizeof(struct shared_area),IPC_CREAT | 0666);
-    shmptr=(struct shared_area*)shmat(shmid,NULL,0);
-    shmptr->rw=0;
-    while(1)
+    int fd;
+    fd=shm_open("Shared", O_CREAT | O_RDWR, 0666);
+    if (fd == -1)
     {
-        while(shmptr->rw != 1);
-        while(shmptr->user2_up == 0)
-        {
-            printf("Recieved from user2 : ");
-            if(strcmp(shmptr->msg,"stop") == 0)
-            {
-                exit(1);
-            }
-            else
-            {
-                puts(shmptr->msg);
-            }
-            shmptr->user1_up=0;
-            shmptr->user2_up=1;
-        }
-        printf("User 1 : ");
-        if(strcmp(shmptr->msg,"stop") == 0)
-        {
-            exit(1);
-        }
-        else
-        {
-            fgets(shmptr->msg,sizeof(shmptr->msg),stdin);
-        }
-        shmptr->rw=0;
+        perror("shm_open");
+        exit(1);
     }
-    shmctl(shmid,IPC_RMID,NULL);
+
+    if(ftruncate(fd, SIZE) == -1)
+    {
+        perror("ftruncate");
+        exit(1);
+    }
+
+    mptr=mmap(mptr, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,fd,0);
+    if(mptr == MAP_FAILED)
+    {
+        perror("mmap");
+        exit(1);
+    }
+    mptr->flag=1;
+    mptr->turn=1;
+    while(mptr->flag == 1)
+    {
+        while(mptr->turn != 1);
+        if(mptr->user2_up == 1)
+        {
+            printf("Message from USER2 : %s\n",mptr->msg);
+            mptr->user2_up=0;
+        }
+        if(mptr->user1_up == 0 && mptr->user2_up!=1)
+        {
+            printf("Enter the message (USER1) : ");
+            scanf(" %[^\n]",mptr->msg);
+            if(strcmp(mptr->msg,"stop") == 0)
+            {
+                mptr->flag=0;
+            }
+            mptr->user1_up=1;
+        }
+        mptr->turn=2;
+    }
+    if(munmap(mptr, SIZE) == -1)
+    {
+        perror("munmap");
+        exit(1);
+    }
+    close(fd);
+    if(shm_unlink("Shared") == -1)
+    {
+        perror("shm_unlink");
+        exit(1);
+    }
     return 0;
 }
+
